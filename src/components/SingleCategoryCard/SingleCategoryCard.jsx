8e4de719b0830ac7e07474b0bd71f5e6eb4482f6
Serg <serega101617@gmail.com>
2024-03-07 21:40:51 +0100

    added CategoriesPage and CategoriesBlock on the HomePage, fix footers references

--- src/components/SingleCategoryCard/SingleCategoryCard.jsx
+++ src/components/SingleCategoryCard/SingleCategoryCard.jsx
@@ -1,11 +1,13 @@
 import GoodsCategoriesTitle from '../GoodsCategoriesTitle/GoodsCategoriesTitle';
 import classes from './SingleCategoryCard.module.css';
 
-const SingleCategoryCard = ({ title, image }) => {
-  console.log(image);
+const SingleCategoryCard = ({ title, image, size }) => {
   return (
-    <div className={classes.wrapper}>
-      <img src={`http://localhost:3333/${image}`} alt={title} />
+    <div className={classes.wrap}>
+      <div
+        className={size ? classes.mainPageWrapper : classes.wrapper}
+        style={{ backgroundImage: `url('http://localhost:3333/${image}')` }}
+      ></div>
       <GoodsCategoriesTitle text={title} />
     </div>
   );
