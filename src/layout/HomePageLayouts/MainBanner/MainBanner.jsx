53fcb016fc8ee8a94b3b876926284d27d371dc78
Natalia <natalia.besan1996@gmail.com>
2024-04-11 18:33:50 +0200

    modifierd

--- src/layout/HomePageLayouts/MainBanner/MainBanner.jsx
+++ src/layout/HomePageLayouts/MainBanner/MainBanner.jsx
@@ -1,5 +1,8 @@
+
+import { Link } from "react-router-dom";
 import MainBannerButton from "../../../UI/MainBannerButton/MainBannerButton";
 import classes from "./MainBanner.module.css";
+import { ROUTES } from "../../../utils/routes";
 
 export const MainBanner = () => {
   return (
@@ -9,7 +12,9 @@ export const MainBanner = () => {
           <h1 className={classes.title}>
             Amazing Discounts on Garden Products!
           </h1>
-          <MainBannerButton text="Check out" />
+          <Link to={`${ROUTES.ALLPRODUCTS}?category=2`}>
+            <MainBannerButton text="Check out" />
+          </Link>
         </div>
       </div>
     </div>
