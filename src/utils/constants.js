8f3b2ddef13478149759f4a599dff7ff738c420b
olena-star <122024787+olena-star@users.noreply.github.com>
2024-04-16 15:43:44 +0200

    change name Form Block

---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://telran-backend-1.onrender.com';
---
+++ src/utils/routes.js
@@ -0,0 +1,13 @@
+export const ROUTES = {
+    HOME: '/',
+    CATEGORIES: '/categories',
+    ALLPRODUCTS: '/allproducts',
+    ALLSALES: '/allsales',
+    PRODUCT: '/products/:id',
+    CATEGORY: '/categories/:id',
+    CART: '/cart',
+    NOTFOUNDPAGE: '/*',
+    ORDER_DISCOUNT: '/sale/send',
+    ORDER_PRODUCT: '/order/send',
+  };
+  
